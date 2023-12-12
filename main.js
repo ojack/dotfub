334757aa4d5e97e4b5db013beca2991f78e11d2d
ojack <ojackson@gmail.com>
2023-12-12 16:59:59 -0600

    working main.js lib

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 import Squirm from './Squirm.js'
-import { } from './actions.js'
+import actions from './actions.js'
 
+const { cloneSquirm } = actions
 
-const actionsToExport = {}
-Object.entries(squirmActionsThatNeedCloning).forEach(([name, action]) => {
-    actionsToExport[name] = (s, payload) => {
-        const clone = cloneSquirm(s)
-        action(clone)(payload)
-        return clone
+// object containing public facing library of squirm functions
+const squirmActions = {}
+Object.entries(actions).forEach(([name, action]) => {
+    // Generate standalone action function from each action
+    // if function name starts with '_', should be cloned and modified before passing to function. 
+    // i.e. the public-facing function `addTick()` will call the private function `_addTick()`
+    let newName = name
+    if(newName.indexOf('_') === 0) {
+        newName = newName.substring(1)
+        squirmActions[newName] = (s, payload) => {
+            const clone = cloneSquirm(s)
+            action(clone, payload)
+            return clone
+        }
+    } else {
+        squirmActions[newName] = action
+    }
+
+    // add method to Squirm prototype
+    Squirm.prototype[newName] = function (payload) {
+        return action(this, payload)
     }
 })
 
-Object.entries(squirmActions).forEach(([name, action]) => {
-    // add as method to squirm class
-    Squirm.prototype[name] = function (data) {
-        return action(this)(data)
-    }
-})
-
-Object.entries(squirmActionsThatNeedCloning).forEach(([name, action]) => {
-    // add as method to squirm class
-    Squirm.prototype[name] = function (data) {
-        return action(this)(data)
-    }
-    // generate user-facing method
-})
+export { Squirm, squirmActions }
