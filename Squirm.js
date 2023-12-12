469e3bc57f06e951b8af3b9ab7f7179bfbbc7b13
nbriz <nickbriz@protonmail.com>
2023-12-12 12:00:34 -0600

    added Squirm class

---
+++ Squirm.js
@@ -0,0 +1,36 @@
+class Squirm {
+  constructor () {
+    this.metadata = {
+      version: '1.0.0',
+      liscend: 'GPLv3'
+      // etc...
+    }
+    this.data = {}
+  }
+
+  set min (v) {
+    console.warn('Squirm: min is a read only property')
+  }
+
+  get min () {
+    const min = {}
+    Object.keys(this.data).forEach(c => {
+      min[c] = Math.min(...this.data[c])
+    })
+    return min
+  }
+
+  set max (v) {
+    console.warn('Squirm: max is a read only property')
+  }
+
+  get max () {
+    const max = {}
+    Object.keys(this.data).forEach(c => {
+      max[c] = Math.max(...this.data[c])
+    })
+    return max
+  }
+}
+
+export default Squirm
