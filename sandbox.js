5988f2f9ed3f33fb4fda284afc1e63cc6c186f8d
ojack <ojackson@gmail.com>
2023-12-12 13:19:46 -0600

    added live-server

---
+++ sandbox.js
@@ -0,0 +1,17 @@
+
+
+const addTickFunctional = (Squirm) => (data) => {
+// 
+}
+
+class Squrim {
+
+}
+
+Squirm.prototype.addTick = addTickFunctional(Squirm.prototype) // returns a funtions that directly 
+
+
+addTick(Squirm, data)
+
+
+addTickFunctional(Squirm)(data)
