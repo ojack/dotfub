d530adfdd94f559eab1bb98d5efd4eb68855d345
nbriz <nickbriz@protonmail.com>
2023-12-14 16:31:26 -0600

    Fubble now has "bounds"

--- libraries/Fubble.js
+++ libraries/Fubble.js
@@ -2,41 +2,51 @@
 // this.bounds.x = [0, 100] user can set
 // this.bounds.t = [0, 1000]  user can not set (should be calculated)
 // add bounds to constructor
+
+/*
+
+const f = new Fubble({
+  metadata: {},
+  columns: ['z'],
+  bounds: {
+    x: [0, 100],
+    x: { min: 0, max: 100 }
+    y: [0, 100],
+    z: [0, 100]
+  }
+})
+
+f.bounds.x.min
+f.min.x
+*/
 class Fubble {
-  constructor ({ metadata = {}, columns = [] } = {}) {
+  constructor ({ metadata = {}, columns = [], bounds = {} } = {}) {
     this.metadata = Object.assign({
       version: '1.0.0',
       license: 'GPLv3'
     }, metadata)
-    // defaults to t, x, and y. later we can add a flag for renaming or removing x and y
+
+    // setup columns, defaults to t, x, and y.
+    // later we can add a flag for renaming or removing x and y
     this.data = { t: [], x: [], y: [] }
     columns.forEach((col) => {
       this.data[col] = []
     })
-  }
 
-  set min (v) {
-    console.warn('Fubble: min is a read only property')
-  }
-
-  get min () {
-    const min = {}
-    Object.keys(this.data).forEach(c => {
-      min[c] = Math.min(...this.data[c])
+    // setup bounds
+    this.bounds = { t: {}, x: {}, y: {} }
+    Object.entries(bounds).map(([key, value]) => {
+      if (typeof this.data[key] === 'undefined') {
+        this.data[key] = []
+      }
+      // todo: type validation or Object.assign() w/defaults
+      // setup min/max
+      this.bounds[key] = value
     })
-    return min
-  }
 
-  set max (v) {
-    console.warn('Fubble: max is a read only property')
-  }
-
-  get max () {
-    const max = {}
-    Object.keys(this.data).forEach(c => {
-      max[c] = Math.max(...this.data[c])
-    })
-    return max
+    // if (typeof arg === 'string' && this.stringToFubble) {
+    //   this.stringToFubble()
+    // }
   }
 }
 
